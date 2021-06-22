ed6882b080cf82c418608b6ffbd85927d3fbfc89
Mikey Robinson <mikeyrobinson@mmc.school.nz>
2021-06-23 10:34:36 +1200

    dice roll added

--- COS Assesment/Form1.Designer.cs
+++ COS Assesment/Form1.Designer.cs
@@ -29,20 +29,98 @@ namespace COS_Assesment
         /// </summary>
         private void InitializeComponent()
         {
+            this.mainPnl = new System.Windows.Forms.Panel();
+            this.piece1 = new System.Windows.Forms.PictureBox();
+            this.PicDice = new System.Windows.Forms.PictureBox();
+            this.PicHeader = new System.Windows.Forms.PictureBox();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.mainPnl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.piece1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicDice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicHeader)).BeginInit();
             this.SuspendLayout();
+            // 
+            // mainPnl
+            // 
+            this.mainPnl.BackgroundImage = global::COS_Assesment.Properties.Resources.board_image;
+            this.mainPnl.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.mainPnl.Controls.Add(this.piece1);
+            this.mainPnl.Location = new System.Drawing.Point(12, 12);
+            this.mainPnl.Name = "mainPnl";
+            this.mainPnl.Size = new System.Drawing.Size(600, 600);
+            this.mainPnl.TabIndex = 0;
+            this.mainPnl.Paint += new System.Windows.Forms.PaintEventHandler(this.mainPnl_Paint);
+            // 
+            // piece1
+            // 
+            this.piece1.BackColor = System.Drawing.Color.Transparent;
+            this.piece1.BackgroundImage = global::COS_Assesment.Properties.Resources.piece1;
+            this.piece1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.piece1.Location = new System.Drawing.Point(29, 538);
+            this.piece1.Name = "piece1";
+            this.piece1.Size = new System.Drawing.Size(27, 50);
+            this.piece1.TabIndex = 3;
+            this.piece1.TabStop = false;
+            // 
+            // PicDice
+            // 
+            this.PicDice.BackgroundImage = global::COS_Assesment.Properties.Resources.dice1;
+            this.PicDice.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PicDice.Location = new System.Drawing.Point(730, 274);
+            this.PicDice.Name = "PicDice";
+            this.PicDice.Size = new System.Drawing.Size(107, 104);
+            this.PicDice.TabIndex = 1;
+            this.PicDice.TabStop = false;
+            // 
+            // PicHeader
+            // 
+            this.PicHeader.BackgroundImage = global::COS_Assesment.Properties.Resources.header;
+            this.PicHeader.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PicHeader.Location = new System.Drawing.Point(660, 12);
+            this.PicHeader.Name = "PicHeader";
+            this.PicHeader.Size = new System.Drawing.Size(250, 153);
+            this.PicHeader.TabIndex = 2;
+            this.PicHeader.TabStop = false;
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.BackColor = System.Drawing.Color.Chartreuse;
+            this.btnRoll.Font = new System.Drawing.Font("Algerian", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRoll.Location = new System.Drawing.Point(698, 465);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(183, 42);
+            this.btnRoll.TabIndex = 3;
+            this.btnRoll.Text = "ROLL!";
+            this.btnRoll.UseVisualStyleBackColor = false;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(952, 522);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(952, 648);
+            this.Controls.Add(this.btnRoll);
+            this.Controls.Add(this.PicHeader);
+            this.Controls.Add(this.PicDice);
+            this.Controls.Add(this.mainPnl);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.mainPnl.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.piece1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicDice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicHeader)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel mainPnl;
+        private System.Windows.Forms.PictureBox piece1;
+        private System.Windows.Forms.PictureBox PicDice;
+        private System.Windows.Forms.PictureBox PicHeader;
+        private System.Windows.Forms.Button btnRoll;
     }
 }
 
--- COS Assesment/Form1.cs
+++ COS Assesment/Form1.cs
@@ -8,13 +8,52 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace COS_Assesment
 {
     public partial class Form1 : Form
     {
+        int Roll = 0;
+        Random rollRnd = new Random();
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void mainPnl_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            Roll = rollRnd.Next(1, 7);
+
+            if (Roll == 1)
+            {
+
+            }
+            else if (Roll == 2)
+            {
+
+            }
+            else if (Roll == 3)
+            {
+
+            }
+            else if (Roll == 4)
+            {
+
+            }
+            else if (Roll == 5)
+            {
+
+            }
+            else if (Roll == 6);
+
+        }
     }
+
 }
